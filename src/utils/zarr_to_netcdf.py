70360f41976c0f37ca7f13e285a34797fe1ae94f
Masha Liukis <mliukis@jpl.nasa.gov>
2021-07-16 13:55:14 -0700

    Reorganize the code so it can be called by other modules; Fix some encoding keys

--- src/utils/zarr_to_netcdf.py
+++ src/utils/zarr_to_netcdf.py
@@ -8,35 +8,27 @@ Convert Zarr data stored in ZarrStoreName to the NetCDF file NetCDFFileName.
 """
 
 import argparse
+import logging
 import timeit
 import warnings
 import xarray as xr
 
-from itscube_types import Coords, DataVars
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
 
 
-if __name__ == '__main__':
-    warnings.filterwarnings('ignore')
-
-    # Command-line arguments parser
-    parser = argparse.ArgumentParser(epilog='\n'.join(__doc__.split('\n')[1:]),
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-i', '--input', type=str, required=True,
-                        help="Input Zarr store directory.")
-    parser.add_argument('-o', '--output', type=str, required=True,
-                        help="NetCDF filename to store data to.")
-    parser.add_argument('-e', '--engine', type=str, required=False, default='h5netcdf',
-                        help="NetCDF engine to use to store NetCDF data to the file.")
-
-    args = parser.parse_args()
-
+def main(input_file: str, output_file: str, nc_engine: str):
+    """
+    Convert datacube Zarr store to NetCDF format.
+    """
     start_time = timeit.default_timer()
     # Don't decode time delta's as it does some internal conversion based on
     # provided units
-    ds_zarr = xr.open_zarr(args.input, decode_timedelta=False)
+    ds_zarr = xr.open_zarr(input_file, decode_timedelta=False)
 
     time_delta = timeit.default_timer() - start_time
-    print(f"Read Zarr {args.input} (took {time_delta} seconds)")
+    logging.info(f"Read Zarr {input_file} (took {time_delta} seconds)")
 
     compression = {"zlib": True, "complevel": 2, "shuffle": True}
     encoding = {}
@@ -95,8 +87,8 @@ if __name__ == '__main__':
         'vyp_error_modeled':         {'_FillValue': -32767.0, 'dtype': 'double'},
         'vyp_error_slow':            {'_FillValue': -32767.0, 'dtype': 'double'},
         'vyp_stable_shift':          {'_FillValue': -32767.0, 'dtype': 'double'},
-        'vxp_stable_shift_slow':     {'_FillValue': -32767.0, 'dtype': 'double'},
-        'vxp_stable_shift_mask':     {'_FillValue': -32767.0, 'dtype': 'double'},
+        'vyp_stable_shift_slow':     {'_FillValue': -32767.0, 'dtype': 'double'},
+        'vyp_stable_shift_mask':     {'_FillValue': -32767.0, 'dtype': 'double'},
         'vp':                        {'_FillValue': -32767.0, 'dtype': 'short'},
         'vp_error':                  {'_FillValue': -32767.0, 'dtype': 'short'},
         'acquisition_date_img1':     {'_FillValue': None, 'units': 'days since 1970-01-01'},
@@ -192,8 +184,8 @@ if __name__ == '__main__':
         'autoRIFT_software_version'
     ]
 
-    if DataVars.MAP_SCALE_CORRECTED in ds_zarr:
-        encode_data_vars.append(DataVars.MAP_SCALE_CORRECTED)
+    if 'map_scale_corrected' in ds_zarr:
+        encode_data_vars.append('map_scale_corrected')
 
     # Set up compression for each of the data variables
     for each in encode_data_vars:
@@ -201,9 +193,26 @@ if __name__ == '__main__':
 
     start_time = timeit.default_timer()
     ds_zarr.to_netcdf(
-        args.output,
-        engine=args.engine,
-        encoding = encoding
+        output_file,
+        engine=nc_engine,
+        encoding=encoding
     )
     time_delta = timeit.default_timer() - start_time
-    print(f"Wrote dataset to NetCDF file {args.output} (took {time_delta} seconds)")
+    logging.info(f"Wrote dataset to NetCDF file {output_file} (took {time_delta} seconds)")
+
+
+if __name__ == '__main__':
+    warnings.filterwarnings('ignore')
+
+    # Command-line arguments parser
+    parser = argparse.ArgumentParser(epilog='\n'.join(__doc__.split('\n')[1:]),
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-i', '--input', type=str, required=True,
+                        help="Input Zarr store directory.")
+    parser.add_argument('-o', '--output', type=str, required=True,
+                        help="NetCDF filename to store data to.")
+    parser.add_argument('-e', '--engine', type=str, required=False, default='h5netcdf',
+                        help="NetCDF engine to use to store NetCDF data to the file.")
+
+    args = parser.parse_args()
+    main(args.input, args.output, args.engine)
