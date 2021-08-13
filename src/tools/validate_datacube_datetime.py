394e59356f6ba53e980b405d7aa5c08430ba6aa0
Masha Liukis <mliukis@jpl.nasa.gov>
2021-08-13 11:19:12 -0700

    Compare datetime objects at date() and hour level - there is off by one for minute or second error when converting np.datetime64 to datetime.datetime object

--- src/tools/validate_datacube_datetime.py
+++ src/tools/validate_datacube_datetime.py
@@ -6,6 +6,7 @@ Authors: Masha Liukis
 """
 import argparse
 import dask
+import datetime
 from dask.diagnostics import ProgressBar
 import logging
 import numpy as np
@@ -39,12 +40,8 @@ class ValidateDatacubes:
             cubes = [each_cube for each_cube in cubes if each_cube.endswith('.zarr')]
             self.all_datacubes.extend(cubes)
 
-        logging.info(f"Number of cubes: {len(self.all_datacubes)}")
+        logging.info(f"Number of cubes to validate: {len(self.all_datacubes)}")
 
-        # Each granule has corresponding png files: '.png', '_thumb.png',
-        # rename those too
-        # self.all_granules = [each for each in self.all_granules if FixGranulesNames.SUFFIX_TO_REMOVE in each]
-        # logging.info(f"Number of {FixGranulesNames.SUFFIX_TO_REMOVE} granules: {len(self.all_granules)}")
 
     def __call__(self, chunk_size: int, num_dask_workers: int):
         """
@@ -73,7 +70,7 @@ class ValidateDatacubes:
                                        num_workers=num_dask_workers)
 
             for each_result in results[0]:
-                logging.info("-->".join(each_result))
+                logging.info("\n--->".join(each_result))
 
             num_to_fix -= num_tasks
             start += num_tasks
@@ -84,14 +81,13 @@ class ValidateDatacubes:
         Validate datacube
         """
         msgs = [f'Processing {cube_url}']
-        units = 'm'
 
         try:
             #
             cube_store = s3fs.S3Map(root=cube_url, s3=s3_in, check=False)
             with xr.open_dataset(cube_store, decode_timedelta=False, engine='zarr', consolidated=True, chunks={'mid_date': 250}) as ds:
-                mid_dates = [np.datetime_as_string(t, unit=units) for t in ds.mid_date.values]
-                date_center = [np.datetime_as_string(t, unit=units) for t in ds.date_center.values]
+                mid_dates = [t.astype(datetime.datetime) for t in ds.mid_date.values]
+                date_center = [t.astype(datetime.datetime) for t in ds.date_center.values]
 
                 if mid_dates != date_center:
                     msgs.append(f"ERROR: mismatching mid_date and date_center for {cube_url}: ")
@@ -106,8 +102,8 @@ class ValidateDatacubes:
                     granule_urls = ds.granule_url.values
 
                     # Validate each layer's datetime against the one as stored in the datacube
-                    acq_date_img1 = [np.datetime_as_string(t, unit=units) for t in ds.acquisition_date_img1.values]
-                    acq_date_img2 = [np.datetime_as_string(t, unit=units) for t in ds.acquisition_date_img2.values]
+                    acq_date_img1 = [t.astype(datetime.datetime) for t in ds.acquisition_date_img1.values]
+                    acq_date_img2 = [t.astype(datetime.datetime) for t in ds.acquisition_date_img2.values]
 
                     for index, each_url in enumerate(granule_urls):
                         # Read each granule in and compare to the value in datacube
@@ -122,43 +118,38 @@ class ValidateDatacubes:
                         # msgs.append(f"Opening {s3_path}...")
                         with s3_in.open(s3_path, mode='rb') as fhandle:
                             with xr.open_dataset(fhandle, engine=ITSCube.NC_ENGINE) as granule_ds:
-                                granule_date_center = np.datetime64(
-                                    ITSCube.get_data_var_attr(
+                                granule_date_center = ITSCube.get_data_var_attr(
                                         granule_ds,
                                         each_url,
                                         DataVars.ImgPairInfo.NAME,
                                         DataVars.ImgPairInfo.DATE_CENTER,
                                         to_date=True
                                     )
-                                )
 
-                                granule_acq_date_img1 = np.datetime64(
-                                    ITSCube.get_data_var_attr(
+                                granule_acq_date_img1 = ITSCube.get_data_var_attr(
                                         granule_ds,
                                         each_url,
                                         DataVars.ImgPairInfo.NAME,
                                         DataVars.ImgPairInfo.ACQUISITION_DATE_IMG1,
                                         to_date=True
                                     )
-                                )
 
-                                granule_acq_date_img2 = np.datetime64(
-                                    ITSCube.get_data_var_attr(
+                                granule_acq_date_img2 = ITSCube.get_data_var_attr(
                                         granule_ds,
                                         each_url,
                                         DataVars.ImgPairInfo.NAME,
                                         DataVars.ImgPairInfo.ACQUISITION_DATE_IMG2,
                                         to_date=True
                                     )
-                                )
 
-                                if date_center[index] != np.datetime_as_string(granule_date_center, unit=units):
+                                # Compare at date() and hour level as there's a discrepancy of a second when np.datetime64 is converted to datetime
+                                if date_center[index].date() != granule_date_center.date() or date_center[index].hour != granule_date_center.hour:
                                     msgs.append(f"ERROR: date_center[{index}]: cube's {date_center[index]} vs. {granule_date_center}")
 
-                                if acq_date_img1[index] != np.datetime_as_string(granule_acq_date_img1, unit=units):
+                                if acq_date_img1[index].date() != granule_acq_date_img1.date() or acq_date_img1[index].hour != granule_acq_date_img1.hour:
                                     msgs.append(f"ERROR: acq_date_img1[{index}]: cube's {acq_date_img1[index]} vs. {granule_acq_date_img1}")
 
-                                if acq_date_img2[index] != np.datetime_as_string(granule_acq_date_img2, unit=units):
+                                if acq_date_img2[index].date() != granule_acq_date_img2.date() or acq_date_img2[index].hour != granule_acq_date_img2.hour:
                                     msgs.append(f"ERROR: acq_date_img2[{index}]: cube's {acq_date_img2[index]} vs. {granule_acq_date_img2}")
 
         except OverflowError as exc:
