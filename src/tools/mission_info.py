e4499cdc0bb8dedb87f31b0326150c0390993ea3
Masha Liukis <mliukis@jpl.nasa.gov>
2021-11-18 17:56:26 -0800

    Add encoding settings for S1

--- src/tools/mission_info.py
+++ src/tools/mission_info.py
@@ -17,3 +17,19 @@ class Encoding:
         'x':                {'_FillValue': None},
         'y':                {'_FillValue': None}
     }
+
+    SENTINEL1 = {
+        'interp_mask':      {'_FillValue': 0.0, 'dtype': 'ubyte', "zlib": True, "complevel": 2, "shuffle": True},
+        'chip_size_height': {'_FillValue': 0.0, 'dtype': 'ushort', "zlib": True, "complevel": 2, "shuffle": True},
+        'chip_size_width':  {'_FillValue': 0.0, 'dtype': 'ushort', "zlib": True, "complevel": 2, "shuffle": True},
+        'M11':              {'_FillValue': -32767, 'dtype': 'short', "zlib": True, "complevel": 2, "shuffle": True},
+        'M12':              {'_FillValue': -32767, 'dtype': 'short', "zlib": True, "complevel": 2, "shuffle": True},
+        'v':                {'_FillValue': -32767.0, 'dtype': 'short', "zlib": True, "complevel": 2, "shuffle": True},
+        'vx':               {'_FillValue': -32767.0, 'dtype': 'short', "zlib": True, "complevel": 2, "shuffle": True},
+        'vy':               {'_FillValue': -32767.0, 'dtype': 'short', "zlib": True, "complevel": 2, "shuffle": True},
+        'v_error':          {'_FillValue': -32767.0, 'dtype': 'short', "zlib": True, "complevel": 2, "shuffle": True},
+        'va':               {'_FillValue': -32767.0, 'dtype': 'short', "zlib": True, "complevel": 2, "shuffle": True},
+        'vr':               {'_FillValue': -32767.0, 'dtype': 'short', "zlib": True, "complevel": 2, "shuffle": True},
+        'x':                {'_FillValue': None},
+        'y':                {'_FillValue': None}
+    }
