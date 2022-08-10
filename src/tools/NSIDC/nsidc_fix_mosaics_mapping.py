f688af244d1e344a9df8d00a900e933b70a1bf55
Masha Liukis <mliukis@jpl.nasa.gov>
2022-08-09 18:53:43 -0700

    Use Mapping class from standalone module

--- src/tools/NSIDC/nsidc_fix_mosaics_mapping.py
+++ src/tools/NSIDC/nsidc_fix_mosaics_mapping.py
@@ -17,30 +17,7 @@ import xarray as xr
 from itscube_types import DataVars
 from nsidc_vel_image_pairs import NSIDCMeta, NSIDCFormat, get_attr_value
 from nsidc_mosaics import Encoding, NSIDCMosaicFormat
-
-
-class Mapping:
-    """
-    Class to define mapping's attributes.
-    """
-    COORDINATE_AXIS_TYPES = 'CoordinateAxisTypes'
-    COORDINATE_TRANSFORM_TYPE = 'CoordinateTransformType'
-    LONGITUDE_OF_CENTRAL_MERIDIAN = 'longitude_of_central_meridian'
-    LATITUDE_OF_PROJECTION_ORIGIN = 'latitude_of_projection_origin'
-    FALSE_EASTING = 'false_easting'
-    FALSE_NORTHING = 'false_northing'
-    STANDARD_PARALLEL = 'standard_parallel'
-    SPATIAL_PROJ4 = 'spatial_proj4'
-    PROJ4TEXT = 'proj4text'
-    SPATIAL_REF = 'spatial_ref'
-    CRS_WKT = 'crs_wkt'
-    LONGITUDE_OF_CENTRAL_MERIDIAN = 'longitude_of_central_meridian'
-    FALSE_EASTING = 'false_easting'
-    FALSE_NORTHING = 'false_northing'
-    STANDARD_PARALLEL = 'standard_parallel'
-    SEMI_MAJOR_AXIS = 'semi_major_axis'
-    SEMI_MINOR_AXIS ='semi_minor_axis'
-    SCALE_FACTOR_AT_CENTRAL_MERIDIAN = 'scale_factor_at_central_meridian'
+from nsidc_types import Mapping
 
 class FixMosaicsMapping:
     """
