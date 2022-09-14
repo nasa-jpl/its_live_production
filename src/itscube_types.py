da785115f1cde279c578a0b12bd774c04f435b90
Masha Liukis <mliukis@jpl.nasa.gov>
2022-09-14 10:32:24 -0700

    Add EPSG back to the composites filenames

--- src/itscube_types.py
+++ src/itscube_types.py
@@ -368,12 +368,19 @@ def datacube_filename_zarr(epsg_format: str, grid_size: int, mid_x: int, mid_y: 
     """
     return f"{FilenamePrefix.Datacube}_{epsg_format}_G{grid_size:04d}_X{mid_x}_Y{mid_y}.zarr"
 
-def composite_filename_zarr(grid_size: str, mid_x: int, mid_y: int):
+def composite_filename_zarr(epsg_format: str, grid_size: str, mid_x: int, mid_y: int):
     """
     Format filename for the datacube's composite:
-    ITS_LIVE_velocity_120m_X-3250000_Y250000.zarr
+    ITS_LIVE_velocity_EPSG3413_120m_X-3250000_Y250000.zarr
+
+    Inputs:
+    =======
+    epsg_format: String representation of the EPSG code in "EPSGXXXXX" format
+    grid_size: Grid size
+    mid_x: X coordinate of datacube centroid
+    mid_y: Y coordinate of datacube centroid
     """
-    return f"{FilenamePrefix.Composites}_{int(grid_size):03d}m_X{mid_x}_Y{mid_y}.zarr"
+    return f"{FilenamePrefix.Composites}_{epsg_format}_{int(grid_size):03d}m_X{mid_x}_Y{mid_y}.zarr"
 
 def annual_mosaics_filename_nc(grid_size: str, region: str, year_date, version: str):
     """
