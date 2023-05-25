793b8ba8b3d9d465af241e7130e772892b261c59
Masha Liukis <mliukis@jpl.nasa.gov>
2023-05-25 11:03:51 -0700

    Use boto3 instead of process - faster

--- src/tools/remove_v2_L7_granules.py
+++ src/tools/remove_v2_L7_granules.py
@@ -2,12 +2,11 @@
 Helper script to remove original granules from the "its-live-data" S3 bucket.
 S3 paths are provided through input JSON file.
 """
+import boto3
 import dask
 from dask.diagnostics import ProgressBar
-import subprocess
 import os
 import logging
-import s3fs
 import json
 
 # Set up logging
@@ -17,12 +16,10 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 
-S3_PREFIX = 's3://its-live-data'
-
-ENV_COPY = os.environ.copy()
+S3_PREFIX = 'its-live-data'
 
 
-def remove_s3_granule(s3: s3fs.S3FileSystem, s3_path: str, is_dryrun: bool):
+def remove_s3_granule(s3_path: str, is_dryrun: bool):
     """
     Remove granule and corresponding *png files from S3 bucket if they exist.
     This is done in preparation to replace buggy granules with newly generated ones.
@@ -30,36 +27,21 @@ def remove_s3_granule(s3: s3fs.S3FileSystem, s3_path: str, is_dryrun: bool):
     # Use "subprocess" as s3fs.S3FileSystem leaves unclosed connections
     # resulting in as many error messages as there are files in Zarr store
     # to copy
-    granule_path = os.path.join(S3_PREFIX, s3_path)
-
-    msgs = []
+    s3 = boto3.resource('s3')
 
     # There are corresponding browse and thumbprint images to transfer
     for target_ext in [None, '.png', '_thumb.png']:
-        file_path = granule_path
+        file_path = s3_path
 
         # It's an extra file to transfer, replace extension
         if target_ext is not None:
-            file_path = granule_path.replace('.nc', target_ext)
+            file_path = s3_path.replace('.nc', target_ext)
 
-        command_line = [
-            "awsv2", "s3", "rm", "--quiet",
-            file_path
-        ]
-        # logging.info(f'{is_dryrun_str}Removing existing {file_path}: {" ".join(command_line)}')
+        obj = s3.Object(S3_PREFIX, file_path)
+        # logging.info(obj)
 
         if not is_dryrun:
-            command_return = subprocess.run(
-                command_line,
-                env=ENV_COPY,
-                check=False,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT
-            )
-            if command_return.returncode != 0:
-                msgs.append(f"ERROR: failed to remove {file_path}: {command_return.stdout}")
-
-    return msgs
+            _ = obj.delete()
 
 
 if __name__ == '__main__':
@@ -86,8 +68,6 @@ if __name__ == '__main__':
     CHUNK_SIZE = 100
     DASK_WORKERS = 8
 
-    s3 = s3fs.S3FileSystem(anon=True, skip_instance_cache=True)
-
     with open(args.granulesFile) as fh:
         all_granules = json.load(fh)
 
@@ -101,20 +81,13 @@ if __name__ == '__main__':
 
             logging.info(f"Starting tasks {start}:{start+num_tasks}")
             tasks = [
-                dask.delayed(remove_s3_granule)(s3, each, args.dryrun) for each in all_granules[start:start+num_tasks]
+                dask.delayed(remove_s3_granule)(each, args.dryrun) for each in all_granules[start:start+num_tasks]
             ]
             results = None
 
             with ProgressBar():
                 # Display progress bar
                 results = dask.compute(tasks, scheduler="processes", num_workers=DASK_WORKERS)
-
-            msgs = []
-            for each_result in results[0]:
-                msgs.extend(each_result)
-
-            if len(msgs):
-                logging.info("\n-->".join(msgs))
 
             num_to_fix -= num_tasks
             start += num_tasks
