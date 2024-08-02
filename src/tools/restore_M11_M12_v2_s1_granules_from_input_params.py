79aed023a90438bc35645300c820f5501f69a377
Masha Liukis <mliukis@jpl.nasa.gov>
2024-08-02 15:19:49 -0700

    Fix paths for png files

--- src/tools/restore_M11_M12_v2_s1_granules_from_input_params.py
+++ src/tools/restore_M11_M12_v2_s1_granules_from_input_params.py
@@ -301,9 +301,9 @@ class RestoreM11M12Values:
                     target_key = target.replace('.nc', target_ext)
 
                     # Path to original PNG file in the S3 bucket - just copy to new location in s3
-                    source_key = target.replace('.nc', target_ext)
+                    source_key = target_key.replace(RestoreM11M12Values.TARGET_DIR, RestoreM11M12Values.SOURCE_DIR).replace('.nc', target_ext)
 
-                    # msgs.append(f"Uploading {source_key} to {RestoreM11M12Values.BUCKET}: {target_key}")
+                    msgs.append(f"Uploading {source_key} to {RestoreM11M12Values.BUCKET}: {target_key}")
                     source_dict = {
                         'Bucket': RestoreM11M12Values.BUCKET,
                         'Key': source_key
