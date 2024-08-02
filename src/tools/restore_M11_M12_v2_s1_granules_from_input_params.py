16b37c8b1f5d941b07cdc3da80766761c3be81b2
Masha Liukis <mliukis@jpl.nasa.gov>
2024-08-02 14:59:09 -0700

    Remove debug info

--- src/tools/restore_M11_M12_v2_s1_granules_from_input_params.py
+++ src/tools/restore_M11_M12_v2_s1_granules_from_input_params.py
@@ -303,14 +303,13 @@ class RestoreM11M12Values:
                     # Path to original PNG file in the S3 bucket - just copy to new location in s3
                     source_key = target.replace('.nc', target_ext)
 
-                    msgs.append(f"Uploading {source_key} to {RestoreM11M12Values.BUCKET}: {target_key}")
-
+                    # msgs.append(f"Uploading {source_key} to {RestoreM11M12Values.BUCKET}: {target_key}")
                     source_dict = {
                         'Bucket': RestoreM11M12Values.BUCKET,
                         'Key': source_key
                     }
 
-                    # bucket.copy(source_dict, target_key)
+                    bucket.copy(source_dict, target_key)
                     msgs.append(f'Copying {target_ext} to s3')
 
             except ClientError as exc:
