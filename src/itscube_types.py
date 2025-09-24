c9b65f707b9d32b9b10b0ae34a2acd0a3b5c9651
Masha Liukis <mliukis@jpl.nasa.gov>
2025-09-24 15:00:35 -0700

    Update for latest v2 datacube catalog with region IDs

--- src/itscube_types.py
+++ src/itscube_types.py
@@ -686,7 +686,7 @@ class CubeJson:
     URL = 'zarr_url'
     EXIST_FLAG = 'datacube_exist'
     GRANULE_COUNT = 'granule_count'
-    REGION = 'region'
+    REGION = 'region_id'
     REGION_ID = 'M_ID'
     RGI_CODE = 'RGI_CODE'
     DIRECTORY = 'directory'
