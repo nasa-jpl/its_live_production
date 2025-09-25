3f0e6c34955c8920850e9b3e743dc6e56c2326a6
Masha Liukis <mliukis@jpl.nasa.gov>
2025-09-25 09:54:38 -0700

    Add used profile

--- src/aws/utils/cancel_running_jobs.bash
+++ src/aws/utils/cancel_running_jobs.bash
@@ -1,7 +1,11 @@
 #! /bin/bash
-for i in $(aws batch list-jobs --job-queue datacube-ondemand-8vCPU-64GB --job-status running --output text --query jobSummaryList[*].[jobId])
+# Call the script: pass AWS_PROFILE for the account to cancel running Batch jobs for
+# ./cancel_running_jobs.bash saml-pub
+
+
+for i in $(awsv2 --profile saml-pub batch list-jobs --profile $1 --job-queue datacube-spot-8vCPU-64GB --job-status running --output text --query jobSummaryList[*].[jobId])
 do
 echo "Deleting Job: $i"
-aws batch terminate-job --job-id $i --reason "Terminating job."
+awsv2 --profile saml-pub batch terminate-job --profile $1 --job-id $i --reason "Terminating job."
 echo "Job $i deleted"
 done
