a99e23c0e36de20533f2430010d48135d70aca56
Masha Liukis <mliukis@jpl.nasa.gov>
2026-03-26 14:31:46 -0700

    Add option to update number of granules in each cube - useful after cubes updates

--- src/tools/add_composites_url_to_datacube_definition.py
+++ src/tools/add_composites_url_to_datacube_definition.py
@@ -11,7 +11,7 @@ import os
 import sys
 import s3fs
 
-from itscube_types import CubeJson
+from itslive_mosaics_types import GeoJsonVars
 
 
 HTTP_PREFIX = 'https://its-live-data.s3.amazonaws.com'
@@ -60,6 +60,11 @@ if __name__ == '__main__':
         default='catalog_with_composites_v2.1.json',
         help="GeoJson file that stores existing composites URLs [%(default)s]."
     )
+    parser.add_argument(
+        '--updateGranuleCountOnly',
+        action='store_true',
+        help='Update the number of granules for each cube in the catalog'
+    )
 
     args = parser.parse_args()
     logging.info(f"Command-line arguments: {sys.argv}")
@@ -70,21 +75,48 @@ if __name__ == '__main__':
     with open(args.cubeCatalog, 'r') as fhandle:
         cubes = json.load(fhandle)
 
-        num_existing_cubes = len(cubes[CubeJson.FEATURES])
+        num_existing_cubes = len(cubes[GeoJsonVars.features])
         num_existing_composites = 0
 
-        for each_cube in cubes[CubeJson.FEATURES]:
-            properties = each_cube[CubeJson.PROPERTIES]
+        for each_cube in cubes[GeoJsonVars.features]:
+            properties = each_cube[GeoJsonVars.properties]
+
+            if args.updateGranuleCountOnly:
+                cube_url = properties[GeoJsonVars.url]
+                # Update number of granules in existing cube if it changed since
+                # some of the cubes were updated with new granules after the
+                # catalog was created.
+                # Write number of granules for existing cube
+                cube_s3_url_meta = os.path.join(
+                    cube_url.replace(HTTP_PREFIX, S3_PREFIX),
+                    '.zmetadata'
+                )
+
+                # Open the cube's metadata to get number of
+                # granules
+                with s3.open(cube_s3_url_meta, 'r') as fh:
+                    meta = json.load(fh)
+                    each_cube[GeoJsonVars.properties][GeoJsonVars.granule_count] = \
+                        meta['metadata']['mid_date/.zarray']['shape'][0]
+
+                    logging.info(
+                        f"Number of granules: "
+                        f"{each_cube[GeoJsonVars.properties][GeoJsonVars.granule_count]} "
+                        f"for {cube_url}"
+                    )
+
+                # No need to update anything else in the catalog
+                continue
 
             # Replace HTTP URL with HTTPS - fixes access using python and
             # julia
-            properties[CubeJson.URL] = properties[CubeJson.URL].replace(
+            properties[GeoJsonVars.url] = properties[GeoJsonVars.url].replace(
                 'http://', 'https://'
             )
 
-            cube_path, cube_filename = os.path.split(properties[CubeJson.URL])
+            cube_path, cube_filename = os.path.split(properties[GeoJsonVars.url])
 
-            logging.info(f'Cube URL original: {properties[CubeJson.URL]}')
+            logging.info(f'Cube URL original: {properties[GeoJsonVars.url]}')
             logging.info(f'Cube split: {cube_path=} {cube_filename=}')
 
             # ITS_LIVE_vel_EPSG32717_G0120_X750000_Y9850000.zarr
@@ -116,7 +148,7 @@ if __name__ == '__main__':
             else:
                 num_existing_composites += 1
 
-            properties[CubeJson.COMPOSITE_URL] = composite_url
+            properties[GeoJsonVars.composite_url] = composite_url
 
         logging.info(f'Number of catalog cubes {num_existing_cubes=}')
         logging.info(f'Number of catalog composites {num_existing_composites=}')
